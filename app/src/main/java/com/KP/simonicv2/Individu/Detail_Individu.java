94b10533c76eeb4c8d0004fa248ce0629c30a723
muftialfarisy <alfabjp@gmail.com>
2020-09-10 11:10:37 +0700

    update tablayout

--- app/src/main/java/com/KP/simonicv2/Individu/Detail_Individu.java
+++ app/src/main/java/com/KP/simonicv2/Individu/Detail_Individu.java
@@ -8,10 +8,17 @@ import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+import com.KP.simonicv2.PositionFragment;
+import com.KP.simonicv2.Profile.ProfileFragment;
 import com.KP.simonicv2.R;
 import com.KP.simonicv2.Reportc.ReportcFragment;
+import com.KP.simonicv2.Reportm.ReportmFragment;
+import com.KP.simonicv2.Suhu.SuhuFragment;
 import com.KP.simonicv2.TabAdapter;
 import com.google.android.material.tabs.TabLayout;
 
@@ -21,6 +28,11 @@ public class Detail_Individu extends AppCompatActivity {
     private static final String TAG = "detail";
     ViewPager viewPager;
     private TabLayout tabLayout;
+    private ProfileFragment profile;
+    private ReportcFragment reportc;
+    private ReportmFragment reportm;
+    private SuhuFragment suhu;
+    private PositionFragment posisi;
     TextView txtnama;
     private int[] tabIcons = {
             R.drawable.ic_note,
@@ -36,6 +48,11 @@ public class Detail_Individu extends AppCompatActivity {
         txtnama = findViewById(R.id.txt_nama2);
 
         tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        profile = new ProfileFragment();
+        reportc = new ReportcFragment();
+        reportm = new ReportmFragment();
+        suhu = new SuhuFragment();
+        posisi = new PositionFragment();
         tabLayout.addTab(tabLayout.newTab().setText("Profile"));
         tabLayout.addTab(tabLayout.newTab().setText("Report CheckUp"));
         tabLayout.addTab(tabLayout.newTab().setText("Report Masalah"));
@@ -44,12 +61,13 @@ public class Detail_Individu extends AppCompatActivity {
         tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
         final TabAdapter adapter = new TabAdapter(this,getSupportFragmentManager(),
                 tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
+        //viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
+                //viewPager.setCurrentItem(tab.getPosition());
+                setCurrentTabFragment(tab.getPosition());
             }
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
@@ -77,6 +95,35 @@ public class Detail_Individu extends AppCompatActivity {
         String awal = getIntent().getStringExtra("tgl_mulai");
         String selesai = getIntent().getStringExtra("tgl_selesai");
 
+    }
+    private void setCurrentTabFragment(int tabPosition)
+    {
+        switch (tabPosition)
+        {
+            case 0 :
+                replaceFragment(profile);
+
+                break;
+            case 1 :
+                replaceFragment(reportc);
+                break;
+            case 2 :
+                replaceFragment(reportm);
+                break;
+            case 3 :
+                replaceFragment(suhu);
+                break;
+            case 4 :
+                replaceFragment(posisi);
+                break;
+        }
+    }
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.rl_pager, fragment);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.commit();
     }
     }
 
